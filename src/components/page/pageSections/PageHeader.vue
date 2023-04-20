de585d1f9e400dd610d97e3be759b6bf087e1ce1
jamesturner-dev <james@jamesturner.dev>
2023-04-20 15:47:51 -0400

    pre branch

--- src/components/page/pageSections/PageHeader.vue
+++ src/components/page/pageSections/PageHeader.vue
@@ -3,7 +3,7 @@
     <span class="mt-5 block text-center text-sm font-semibold primaryText">
       {{ content.subHeading }}
     </span>
-    <span class="mb-2 block text-center text-2xl font-bold leading-8 tracking-tight text-gray-500">
+    <span class="mb-2 block text-center text-2xl font-bold leading-8 tracking-tight text-slate-600">
       {{ content.heading }}
     </span>
   </h1>
