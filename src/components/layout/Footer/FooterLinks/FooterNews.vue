127f2a7285649dd73b823953e769f5090b400354
jamesturner-dev <james@jamesturner.dev>
2023-04-20 13:59:04 -0400

    Fixed Headlin3s Links

--- src/components/layout/Footer/FooterLinks/FooterNews.vue
+++ src/components/layout/Footer/FooterLinks/FooterNews.vue
@@ -2,7 +2,7 @@
   <section class="footerWrapper">
     <h3> Latest News: </h3>
     <ul role="list" class="mt-4 space-y-4">
-      <li v-for="item in categories" :key="item.name">
+      <li v-for="item in cat" :key="item.name">
         <a :href="item.href" class="footerItem">
           {{ item.name }}
         </a>
@@ -12,9 +12,9 @@
 </template>
 
 <script setup>
-const categories = [
-  { name: "Technology", href: "https://headlin3s.com/categories/Tech" },
-  { name: "The News", href: "https://headlin3s.com/categories/News" },
+const cat = [
+  { name: "Technology", href: "https://headlin3s.com/cat/Tech" },
+  { name: "The News", href: "https://headlin3s.com/cat/News" },
   { name: "Online Video", href: "https://linkz.network/tag/video" },
   { name: "Software", href: "https://linkz.network/cat/software" },
 ];
