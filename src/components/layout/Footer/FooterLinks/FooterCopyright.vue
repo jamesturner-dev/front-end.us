cdb3556241304dcc41ba42f57eb205ee00e6bfeb
jamesturner-dev <james@jamesturner.dev>
2023-04-21 13:54:38 -0400

    v 0.1.5

--- src/components/layout/Footer/FooterLinks/FooterCopyright.vue
+++ src/components/layout/Footer/FooterLinks/FooterCopyright.vue
@@ -10,7 +10,7 @@
 <script setup>
 import { ref } from "vue";
 const info = ref({
-  version: "0.1.4",
+  version: "0.1.5",
   name: "JamesTurnerDev",
   link: "https://jamesturner.dev/",
   year: new Date().getFullYear(),
