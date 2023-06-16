50cebf6ac6b4976af45fa1e2e76a2d1ac1bd5a88
jamesturner-dev <james@jamesturner.dev>
2023-06-16 19:09:44 -0400

    0.1.6 footer

--- src/components/layout/Footer/FooterLinks/FooterCopyright.vue
+++ src/components/layout/Footer/FooterLinks/FooterCopyright.vue
@@ -1,10 +1,10 @@
 <template>
   <p class="mt-8 text-sm text-gray-500 md:mt-0 md:order-1 py-2">
-      &copy; {{ info.year }}
-      <a class="primaryText hover:text-sky-600" :href="info.link">
-        {{ info.name }}
-      </a> - Some rights reserved. &nbsp; Version {{ info.version }} 
-    </p>
+    &copy; {{ info.year }}
+    <a class="primaryText hover:text-sky-600" :href="info.link">
+      {{ info.name }}
+    </a> - Some rights reserved. &nbsp; Version {{ info.version }}
+  </p>
 </template>
 
 <script setup>
